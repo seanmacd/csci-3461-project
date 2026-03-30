8807b8257e584a332f186d38dad5a8465a5e3281
= <s3anmacd@gmail.com>
2026-03-30 17:54:19 -0300

    Changed theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Tabs, TabsList, TabsPanel, TabsTab} from '@mantine/core'
+import {Badge, Box, Group, Stack, Tabs, TabsList, TabsPanel, TabsTab} from '@mantine/core'
 import {CarIcon} from '@phosphor-icons/react/dist/ssr'
 import {DuckDbProvider} from '../data/duckdb-provider'
 import {DashboardTab} from './Dashboard.tab'
@@ -6,25 +6,45 @@ import {OrdersTab} from './Orders.tab'
 import {PartsTab} from './Parts.tab'
 import {SuppliersTab} from './Suppliers.tab'
 
+type TabDef = {
+  value: string
+  label: string
+}
+
+const tabDefs: TabDef[] = [
+  {value: 'dashboard', label: 'Dashboard'},
+  {value: 'suppliers', label: 'Suppliers'},
+  {value: 'parts', label: 'Parts'},
+  {value: 'orders', label: 'Orders'},
+  {value: 'annualExpenses', label: 'Annual Expenses'},
+  {value: 'budgetProjection', label: 'Budget Projection'}
+]
+
 export default async function Dashboard() {
+  // This should be lazy loaded per tab vs loading everything upfront
   const provider = new DuckDbProvider()
   const suppliers = await provider.getSuppliers()
   const parts = await provider.getParts()
   const orders = await provider.getOrders()
 
+  const tabs = tabDefs.map(def => (
+    <TabsTab key={def.value} value={def.value} py="lg" w="100%">
+      {def.label}
+    </TabsTab>
+  ))
+
   return (
     <Tabs defaultValue="dashboard" orientation="vertical" py="md" h="100vh">
-      <TabsList w={150} style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
-        <div style={{display: 'flex', alignItems: 'center', gap: '0.5rem', padding: '0.5rem'}}>
-          <CarIcon size={32} />
-        </div>
-        <TabsTab value="dashboard">Dashboard</TabsTab>
-        <TabsTab value="suppliers">Suppliers</TabsTab>
-        <TabsTab value="parts">Parts</TabsTab>
-        <TabsTab value="orders">Orders</TabsTab>
-        <TabsTab value="annualExpenses">Annual Expenses</TabsTab>
-        <TabsTab value="budgetProjection">Budget Projection</TabsTab>
-        {/* Future tabs for other data */}
+      <TabsList w={200}>
+        <Stack align="center">
+          <Group gap="xs">
+            <CarIcon size={32} weight="fill" color="var(--mantine-primary-color-filled)" />
+            <Badge size="lg">MUC</Badge>
+          </Group>
+          <Box flex={1} w="100%">
+            {tabs}
+          </Box>
+        </Stack>
       </TabsList>
 
       <TabsPanel value="dashboard">
--- theme.ts
+++ theme.ts
@@ -3,5 +3,6 @@
 import {createTheme} from '@mantine/core'
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  primaryColor: 'violet',
+  defaultRadius: 'md'
 })
